7a545cc590f09409ab6fb5ae1e2c982058398710
Abdiqani Afrah <abdi@Abdiqanis-MacBook-Air.local>
2022-05-09 14:21:20 +0100

    def keyword

--- src/main/scala/VarValLazyValDef.scala
+++ src/main/scala/VarValLazyValDef.scala
@@ -30,13 +30,69 @@ object VarValLazyValDef extends App {
 
   println("---------------------------")
   // Val can be MODIFIED
-
+  // changing the values at index 0 and 2, meaning val objects can be MODIFIED
+  // Its not POSSIBLE to REASSIGN data to an entirely new array
+  // its possible to modify the object which the val references, but its not possible to reassign
   dataVal(0) = "BAY"
   dataVal(2) = "PSG"
   dataVal.foreach(println)
 
 
-  // Def is used to define methods. The code within the method is not evaluated at the time of definition, but instead the method/code is executed when invoked
+  // Def is used to define methods. The code within the method is not evaluated at the time of definition, but instead the method/code is executed when invoked.
+
+  // method that take a input argument and returns (eg. 14/100)
+  def stockPrices: Int = 14
+  def volumes: Int = 100
+
+
+  def finalValues: Int = stockPrices * volumes
+
+  // finalValues to return type string, add .toString
+  def changingToString: String = (stockPrices * volumes).toString
+  println(s"The final value is: $finalValues" )
+
+
+
+
+  def numbers: Int = {
+    println("Returning Stock Prices")
+    99
+  }
+
+  def figures: Int = {
+  println("Returning Volume ")
+    17
+  }
+  // finalvalueAgain stockPrices and stockPrices invoked as the  method has been evaluated or executed
+  def finalvalueAgain: Int = stockPrices * volumes
+  println(s"the final value is: $finalvalueAgain" )
+
+
+
+  def finalData:Array[String] = {
+    // after the method call, then this line is executed and printed to console
+    // Once the method invocation is complete, then the array is printed to screen
+    println("Array is initialized")
+    Array("Google", "Microsoft", "Tesla")
+  }
+  // "Before accessing data" is printed first before "Array is initialized" even though Array is initialized was defined first
+//
+  println("Before accessing data")
+  // method invocation or method call
+  finalData.foreach(println)
+
+  // Every method invocation runs the code associated with the method
+  println("----------------------")
+  finalData.foreach(println)
+
+
+  var y = 15
+  lazy val x = 50
+  // cannot reassign lazy val
+  x = 75
+
+  var finalValue = y * x
+  println(s"Final value is: $finalValue")
 
 
 
